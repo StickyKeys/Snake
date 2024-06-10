8107f81938122e373a4e9a1a5c5e7ef5b99c4b76
Steven Bartkiewicz <sbartkiewicz@gmail.com>
2024-06-10 00:26:06 -0400

    Fixed www script

--- app.js
+++ app.js
@@ -15,8 +15,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen('3000', () => {
-    console.log("Listening on port 3000");
-})
-
 module.exports = app;
